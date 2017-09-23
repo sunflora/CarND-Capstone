b1b1cc6bd501935ff3105582829ebf27e3d924a3
Flora Sun <sunflora@gmail.com>
2017-09-23 20:12:09 +0800

    minor adjustment

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -27,7 +27,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 LOOKAHEAD_WPS = 100 # Number of base_lane we will publish. You can change this number
 MPH_2_mps = 0.44704
 TARGET_VELOCITY_MPS = 10 
-BRAKE_LIMIT_LOWER = 10
+BRAKE_LIMIT_LOWER = 15
 BRAKE_LIMIT_UPPER = 40
 
 class WaypointUpdater(object):
